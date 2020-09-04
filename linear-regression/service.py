d1889184a52a560dd17844097bc51677e32578e1
bavanya <kurrabchoudhry.cse18@itbhu.ac.in>
2020-09-05 01:37:26 +0530

    add splitting data with kfold

--- linear-regression/service.py
+++ linear-regression/service.py
@@ -6,6 +6,7 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer 
+from sklearn.model_selection import KFold
 
 #X,y = datasets.make_regression(n_samples=100, n_features=1, noise =20)
 #X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
@@ -19,8 +20,8 @@ dataset = pd.read_csv("fishermen_mercury.csv")
 
 #storing features and actual results in numpy arrays 
 X= dataset.iloc[:, :-2].values
-Y_methyl_mercury=dataset.iloc[:, 7].values
-Y_total_mercury=dataset.iloc[:, 7].values
+y_methyl_mercury=dataset.iloc[:, 7].values
+y_total_mercury=dataset.iloc[:, 7].values
 
 #applying one-hot encoding for fishermen and fishpart columns
 columnTransformer = ColumnTransformer([('encoder', 
@@ -38,3 +39,16 @@ columnTransformer = ColumnTransformer([('encoder',
 X = np.array(columnTransformer.fit_transform(X), dtype = np.str)
 
 #print(X)
+
+#spliting datset with kfold validation
+kfold = KFold(n_splits=4)
+for train_index, test_index in kfold.split(X):
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y_methyl_mercury[train_index], y_methyl_mercury[test_index]
+    #call the regressor model from another file and find error and append to list
+
+kfold_1 = KFold(n_splits=4)
+for train_index, test_index in kfold_1.split(X):
+    X_train_1, X_test_1 = X[train_index], X[test_index]
+    y_train_1, y_test_1 = y_total_mercury[train_index], y_total_mercury[test_index]
+    #call the regressor model from another file and find error and append to list
