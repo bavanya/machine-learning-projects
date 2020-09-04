27a61e3fe3d748ec348a3e7a399fb088b37d2b05
bavanya <kurrabchoudhry.cse18@itbhu.ac.in>
2020-09-05 00:28:59 +0530

    import dataset and store features and actual results

--- linear-regression/service.py
+++ linear-regression/service.py
@@ -1,11 +1,22 @@
+#importing libraries
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
-X,y = datasets.make_regression(n_samples=100, n_features=1, noise =20)
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
+#X,y = datasets.make_regression(n_samples=100, n_features=1, noise =20)
+#X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
 
-visualize = plt.figure()
-plt.scatter(X, y)
-plt.show()
+#visualize = plt.figure()
+#plt.scatter(X, y)
+#plt.show()
+
+#importing dataset
+dataset = pd.read_csv("fishermen_mercury.csv")
+
+#storing features and actual results in numpy arrays 
+X= dataset.iloc[:, :-2].values
+Y_methyl_mercury=dataset.iloc[:, 7].values
+Y_total_mercury=dataset.iloc[:, 7].values
